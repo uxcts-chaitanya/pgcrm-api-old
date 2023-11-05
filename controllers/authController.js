052e956ec1373221ae1507f36bc817d05e208738
Sai Varadula <svaradula@expediagroup.com>
2023-11-05 22:21:26 +0530

    Dyanamic authentication

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,13 +2,24 @@ const User = require("../models/user");
 const moment = require("moment");
 
 const login = async (req, res) => {
-	console.log(req.body);
-	res.send({
-		id: 1,
-		username: "admin",
-		password: "123456",
-		email: "admin@themesdesign.in",
-	});
+	const user = await User.findOne({ email: req.body.username });
+	console.log(req.body, user);
+	if (!user) {
+		res
+			.status(200)
+			.send({ isAuthenticated: false, message: "Invalid username/password" });
+	} else if (user.password !== req.body.password) {
+		res
+			.status(200)
+			.send({ isAuthenticated: false, message: "Invalid username/password" });
+	} else {
+		const resp = {
+			isAuthenticated: true,
+			message: "User Authenticated",
+			user: user,
+		};
+		res.status(200).send(resp);
+	}
 };
 
 module.exports = {
