eb20589f22eca6b29fe8235dfc8d94e6586ce3fe
chaitanya kandula <chaitanya@chaitanyas-MacBook-Pro.local>
2023-11-07 17:37:02 +0530

    Added Enquiries

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,9 @@ const router = expressRouter();
 
 // auth routes
 const authRouter = require("./authRoutes");
+const enquiriesRouter = require("./enquiriesRoutes");
 router.use("/auth", authRouter);
+router.use(enquiriesRouter);
 
 // default routes
 router.use("/", (req, res) => {
